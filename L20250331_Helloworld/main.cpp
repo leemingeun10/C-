e957ed6abd09bdf008c885430a786374c6fa58c7
leemingeun10 <163983786+leemingeun10@users.noreply.github.com>
2025-03-31 16:56:39 +0900

    baekjun+
    update baekjun

---
+++ L20250331_Helloworld/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//C++로 big integer
+//네이밍 규칙 파스칼케이싱 
+int main()
+{ 
+	int Number1 = 5;
+	int Number2 = 4;	
+
+	float A = 1.5f;
+
+	cout << Number1+Number2<< endl;
+
+	
+
+
+
+	return 0;
+}
