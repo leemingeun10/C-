8422c11ce2d151c30aa358faaf421e32b670b5d5
leemingeun10 <163983786+leemingeun10@users.noreply.github.com>
2025-04-07 10:06:32 +0900

    20250407
    2

--- L20250407/20250407.cpp
+++ L20250407/20250407.cpp
@@ -2,6 +2,7 @@
 #include<Ctime>
 using namespace std;
 
+/* first 
 int main()
 {
 	std::srand(static_cast<unsigned int>(std::time(0)));
@@ -32,6 +33,8 @@ int main()
 
 
 	}
+
+
 	for (int a = 0; a < 52; a++)
 	{
 		cout << numarr[a] << " ";
@@ -43,3 +46,52 @@ int main()
 
 
 }
+
+*/
+//Fisher-Yates Shuffle 알고리즘
+//동적할당 ++ 
+
+
+#include<iostream>
+#include<Ctime>
+using namespace std;
+
+int main()
+{
+	std::srand(static_cast<unsigned int>(std::time(0)));
+	//std::rand() % 100 + 1
+
+	 
+	int NumberOfArr;
+	
+	cin >> NumberOfArr;
+
+	int* numarr = new int[NumberOfArr];
+
+	for(int a = 0 ;a< NumberOfArr; a++)
+	{
+		numarr[a] = a;
+	}
+
+	for (int a = 0; a < NumberOfArr; a++)
+	{
+		int swapnum = rand() % (a + 1);
+
+		swap(numarr[a], numarr[swapnum]);
+	}
+
+	for (int a = 0; a < NumberOfArr; a++)
+	{
+		cout << numarr[a] << " ";
+	}
+
+
+
+	delete[] numarr;
+	return 0;
+}
+
+
+
+
+
